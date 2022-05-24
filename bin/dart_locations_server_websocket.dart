d7ae436143c5be6e114b14924ecfcf77ee9c5046
shep <mark_sheppard_1977@hotmail.com>
2022-05-24 18:02:29 +0100

    Takes in indivial location reports and outputs lastKnownLocations

--- bin/dart_locations_server_websocket.dart
+++ bin/dart_locations_server_websocket.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
+import 'location_report.dart';
 import 'mime.dart' as mime;
 
 final HOST = "127.0.0.1";
@@ -39,6 +41,7 @@ void main(List<String> arguments) {
 class WebSocketHandler {
   Map<String, WebSocket> users = {}; // Map of current users
   Set<WebSocket> connections = {};
+  Map<String, LocationReport> lastKnownLocations = {};
 
   wsHandler(WebSocket ws) {
     ws.listen((message) {
@@ -56,8 +59,26 @@ class WebSocketHandler {
 
   processMessage2(WebSocket ws, String receivedMessage) {
     log("receivedMessage: $receivedMessage");
+    var tokens = receivedMessage.split('#');
+    var messageType = tokens[0];
+    var message = tokens[1];
+    if (messageType == "LOCATION_REPORT") {
+      processMessageLocationReport(message);
+    }
     for (var ws in connections) {
       ws.add("ECHO: $receivedMessage");
+    }
+  }
+
+  processMessageLocationReport(String message) {
+    var lr = LocationReport.fromJson(jsonDecode(message));
+    lastKnownLocations[lr.id] = lr;
+    var allLocationsReport = AllLocationsReport(
+        timestamp: DateTime.now(), locations: lastKnownLocations);
+    var outgoingMessage = "LASTLOCATIONS#" + jsonEncode(allLocationsReport);
+    print("sendingMessage: " + outgoingMessage);
+    for (var ws in connections) {
+      ws.add(outgoingMessage);
     }
   }
 
